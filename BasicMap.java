b0b658305ee6d6c30e21f8ba1c9fd87f0586ee9d
Parimala Tejaswi <parimalatejaswi6@gmail.com>
2025-06-12 14:48:19 +0530

    Add files via upload

--- BasicMap.java
+++ BasicMap.java
@@ -2,9 +2,10 @@ import java.util.*;
 public class BasicMap {
     public static void main(String[] args) {
         Map<Integer,String> map = new HashMap<>();
-        map.put(200,"ABC");
-        map.put(250,"XYZ");
+        map.put(250,"ABC");
+        map.put(200,"XYZ");
         map.put(300,"PQR");
+        //automatically sorted 
         for(Map.Entry<Integer,String> entry: map.entrySet()){
             System.out.println(entry.getKey()+" -> "+entry.getValue());
         }
